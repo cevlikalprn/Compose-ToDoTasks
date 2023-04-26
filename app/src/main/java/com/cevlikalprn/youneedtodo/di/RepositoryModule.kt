12e87e9b65d6adc86f83b542e957e5f2b2865b81
alperencevlik <alperen.cevlik@leanscale.com>
2023-04-26 20:59:38 +0300

    create RepositoryModule.kt

--- app/src/main/java/com/cevlikalprn/youneedtodo/di/RepositoryModule.kt
+++ app/src/main/java/com/cevlikalprn/youneedtodo/di/RepositoryModule.kt
@@ -1,4 +1,23 @@
 package com.cevlikalprn.youneedtodo.di
 
+import com.cevlikalprn.youneedtodo.data.local.TodoDao
+import com.cevlikalprn.youneedtodo.data.repository.ToDoRepositoryImpl
+import com.cevlikalprn.youneedtodo.domain.repository.ToDoRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideToDoRepository(
+        todoDao: TodoDao
+    ): ToDoRepository {
+        return ToDoRepositoryImpl(todoDao)
+    }
 }
