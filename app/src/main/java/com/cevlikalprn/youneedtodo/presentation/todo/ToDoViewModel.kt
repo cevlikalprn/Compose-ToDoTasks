c6e70b37670f77ba764cffd0554f5f1345ac830a
alperencevlik <alperen.cevlik@dcafood.com>
2023-11-18 14:14:24 +0300

    moved viewmodel class to todo package

--- app/src/main/java/com/cevlikalprn/youneedtodo/presentation/todo/ToDoViewModel.kt
+++ app/src/main/java/com/cevlikalprn/youneedtodo/presentation/todo/ToDoViewModel.kt
@@ -1,4 +1,4 @@
-package com.cevlikalprn.youneedtodo.presentation.viewmodel
+package com.cevlikalprn.youneedtodo.presentation.todo
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
