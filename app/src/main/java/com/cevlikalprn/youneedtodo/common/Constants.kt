d42fe0bd691700addcfb8517c6b18a280a31b25f
alperencevlik <alperen.cevlik@leanscale.com>
2023-12-23 14:33:36 +0300

    add new constants

--- app/src/main/java/com/cevlikalprn/youneedtodo/common/Constants.kt
+++ app/src/main/java/com/cevlikalprn/youneedtodo/common/Constants.kt
@@ -2,13 +2,19 @@ package com.cevlikalprn.youneedtodo.common
 
 object Constants {
 
+    // Database
     const val DATABASE_VERSION = 1
     const val DATABASE_TABLE = "todo_table"
     const val DATABASE_NAME = "todo_database"
 
+    // Screen
     const val LIST_SCREEN = "list/{action}"
     const val TASK_SCREEN = "task/{taskId}"
-
     const val LIST_ARGUMENT_KEY = "action"
     const val TASK_ARGUMENT_KEY = "taskId"
+    const val ADD_TASK_ID = -1
+
+    // General
+    const val EMPTY_STRING = ""
+
 }
