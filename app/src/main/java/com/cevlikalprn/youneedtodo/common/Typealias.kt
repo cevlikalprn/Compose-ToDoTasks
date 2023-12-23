05bb769bec6ac63c8cccd1f943a8714194e9f0be
alperencevlik <alperen.cevlik@leanscale.com>
2023-12-23 14:33:26 +0300

    craete typealiases

---
+++ app/src/main/java/com/cevlikalprn/youneedtodo/common/Typealias.kt
@@ -0,0 +1,8 @@
+package com.cevlikalprn.youneedtodo.common
+
+// Click
+typealias SimpleOnClick = () -> Unit
+
+// Action
+typealias NavigateToTaskScreenAction = (Int) -> Unit
+typealias NavigateToListScreenAction = (Action) -> Unit
