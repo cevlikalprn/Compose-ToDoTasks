f330c1c0c7798ed6e85dd190a6c42534c607e6d8
alperencevlik <alperen.cevlik@leanscale.com>
2023-12-23 14:40:13 +0300

    remove unused imports

--- app/src/main/java/com/cevlikalprn/youneedtodo/presentation/navigation/destination/TaskComposable.kt
+++ app/src/main/java/com/cevlikalprn/youneedtodo/presentation/navigation/destination/TaskComposable.kt
@@ -4,7 +4,6 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import com.cevlikalprn.youneedtodo.common.Action
 import com.cevlikalprn.youneedtodo.common.Constants.TASK_ARGUMENT_KEY
 import com.cevlikalprn.youneedtodo.common.Constants.TASK_SCREEN
 import com.cevlikalprn.youneedtodo.common.NavigateToListScreenAction
