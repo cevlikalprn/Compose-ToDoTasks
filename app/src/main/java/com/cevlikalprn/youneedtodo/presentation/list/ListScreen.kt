9ca3460723450c3f4a4e2691dea0d000b26aaf3e
alperencevlik <alperen.cevlik@leanscale.com>
2023-12-23 14:39:20 +0300

    create List Screen Preview

--- app/src/main/java/com/cevlikalprn/youneedtodo/presentation/list/ListScreen.kt
+++ app/src/main/java/com/cevlikalprn/youneedtodo/presentation/list/ListScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.cevlikalprn.youneedtodo.common.Constants.ADD_TASK_ID
 import com.cevlikalprn.youneedtodo.common.NavigateToTaskScreenAction
 import com.cevlikalprn.youneedtodo.presentation.uipack.AppFloatingActionButton
@@ -27,3 +28,12 @@ fun ListScreen(
     )
 }
 
+@Composable
+@Preview
+fun ListScreenPreview() {
+    ListScreen(
+        navigateToTaskScreenAction = {
+            // do nothing
+        }
+    )
+}
