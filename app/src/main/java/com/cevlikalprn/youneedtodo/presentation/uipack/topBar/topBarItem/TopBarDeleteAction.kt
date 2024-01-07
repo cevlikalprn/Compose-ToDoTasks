60c323c18d6cd65f38bee38d9e98c6c93eb77f7f
alperencevlik <alperen.cevlik@dcafood.com>
2024-01-07 16:25:19 +0300

    please commit

--- app/src/main/java/com/cevlikalprn/youneedtodo/presentation/uipack/topBar/topBarItem/TopBarDeleteAction.kt
+++ app/src/main/java/com/cevlikalprn/youneedtodo/presentation/uipack/topBar/topBarItem/TopBarDeleteAction.kt
@@ -3,55 +3,38 @@ package com.cevlikalprn.youneedtodo.presentation.uipack.topBar.topBarItem
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.cevlikalprn.youneedtodo.R
-import com.cevlikalprn.youneedtodo.common.Constants
 import com.cevlikalprn.youneedtodo.common.TopBarDeleteOnClick
 import com.cevlikalprn.youneedtodo.presentation.theme.LARGE_PADDING
 import com.cevlikalprn.youneedtodo.presentation.theme.Typography
-import com.cevlikalprn.youneedtodo.presentation.theme.appTopBarContentColor
+import com.cevlikalprn.youneedtodo.presentation.uipack.iconButton.AppThreeDotIconButton
 
 @Composable
 fun TopBarDeleteAction(
-    onDeleteClick: TopBarDeleteOnClick,
-    iconTint: Color = MaterialTheme.colors.appTopBarContentColor
+    onDeleteClick: TopBarDeleteOnClick
 ) {
     var expanded by remember { mutableStateOf(false) }
-    IconButton(
-        onClick = { expanded = true }
-    ) {
-        ThreeDotIcon(tint = iconTint)
-        ExpandableDropDown(
-            expanded = expanded,
-            onDismissRequest = { expanded = false },
-            onDeleteClick = {
-                expanded = false
-                onDeleteClick()
-            }
-        )
-    }
-}
-
-@Composable
-private fun ThreeDotIcon(tint: Color) {
-    Icon(
-        imageVector = Icons.Filled.MoreVert,
-        contentDescription = Constants.EMPTY_STRING,
-        tint = tint
+    AppThreeDotIconButton(
+        onThreeDotIconClick = { expanded = true },
+        onIconAction = {
+            ExpandableDropDown(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                onDeleteClick = {
+                    expanded = false
+                    onDeleteClick()
+                }
+            )
+        }
     )
 }
 
