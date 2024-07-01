efccfaeaeb3de55c5a307b585b75e62b86371946
alperencevlik <alperen.cevlik@dcafood.com>
2024-07-01 21:22:21 +0300

    create unit test for UpdateTaskUseCase

---
+++ app/src/test/java/com/cevlikalprn/youneedtodo/domain/useCase/UpdateTaskUseCaseTest.kt
@@ -0,0 +1,61 @@
+package com.cevlikalprn.youneedtodo.domain.useCase
+
+import com.cevlikalprn.youneedtodo.data.FakeToDoRepository
+import com.cevlikalprn.youneedtodo.data.mapper.TaskEntityMapper
+import com.cevlikalprn.youneedtodo.domain.model.Priority
+import com.cevlikalprn.youneedtodo.domain.model.ToDoTask
+import com.cevlikalprn.youneedtodo.domain.model.ToDoTaskEntity
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class UpdateTaskUseCaseTest {
+
+    private lateinit var toDoRepository: FakeToDoRepository
+    private lateinit var updateTaskUseCase: UpdateTaskUseCase
+
+    private val taskId = 1
+
+    @Before
+    fun setUp() {
+        toDoRepository = FakeToDoRepository()
+        updateTaskUseCase = UpdateTaskUseCase(
+            toDoRepository = toDoRepository,
+            taskEntityMapper = TaskEntityMapper()
+        )
+
+        val taskToAdd = ToDoTaskEntity(
+            id = taskId,
+            title = "New Task Title",
+            description = "New task description",
+            priority = Priority.HIGH
+        )
+
+        runBlocking {
+            toDoRepository.addTask(taskToAdd)
+        }
+    }
+
+    @Test
+    fun `Update a task`() = runBlocking {
+        val updatedTaskTitle = "Updated Task Title"
+        val updatedTaskDescription = "Updated Task Description"
+        val updatedPriority = Priority.LOW
+
+        updateTaskUseCase(
+            ToDoTask(
+                id = taskId,
+                title = updatedTaskTitle,
+                description = updatedTaskDescription,
+                priority = updatedPriority
+            )
+        )
+
+        toDoRepository.getSelectedTask(taskId).collectLatest {
+            assert(it?.title == updatedTaskTitle)
+            assert(it?.description == updatedTaskDescription)
+            assert(it?.priority == updatedPriority)
+        }
+    }
+}
