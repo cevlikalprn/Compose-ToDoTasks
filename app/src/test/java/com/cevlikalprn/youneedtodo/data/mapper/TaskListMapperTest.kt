eb7641b8984906513287a44819ce929b71ee6db2
alperencevlik <alperen.cevlik@dcafood.com>
2024-07-21 15:06:18 +0300

    create test for TaskListMapper

---
+++ app/src/test/java/com/cevlikalprn/youneedtodo/data/mapper/TaskListMapperTest.kt
@@ -0,0 +1,32 @@
+package com.cevlikalprn.youneedtodo.data.mapper
+
+import com.cevlikalprn.youneedtodo.domain.model.Priority
+import com.cevlikalprn.youneedtodo.domain.model.ToDoTaskEntity
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class TaskListMapperTest {
+
+    private lateinit var taskListMapper: TaskListMapper
+
+    @Before
+    fun setup() {
+        taskListMapper = TaskListMapper()
+    }
+
+    @Test
+    fun taskListMapper_toDoTaskEntityTransformsIntoToDoTask_returnsTodoTaskList() {
+        val toDoTaskEntityList = listOf(
+            ToDoTaskEntity(1, "title 1", "desc 1", Priority.NONE),
+            ToDoTaskEntity(2, "title 2", "desc 2", Priority.LOW)
+        )
+        val todoTaskList = taskListMapper(toDoTaskEntityList)
+        todoTaskList.forEachIndexed { index, toDoTask ->
+            assertEquals(toDoTaskEntityList[index].id, toDoTask.id)
+            assertEquals(toDoTaskEntityList[index].title, toDoTask.title)
+            assertEquals(toDoTaskEntityList[index].description, toDoTask.description)
+            assertEquals(toDoTaskEntityList[index].priority, toDoTask.priority)
+        }
+    }
+}
