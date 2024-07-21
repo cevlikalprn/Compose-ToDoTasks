75dd12a6b4c44555e927d6d3a14e40557938cd03
alperencevlik <alperen.cevlik@dcafood.com>
2024-07-21 14:49:43 +0300

    create test for TaskEntityMapper

---
+++ app/src/test/java/com/cevlikalprn/youneedtodo/data/mapper/TaskEntityMapperTest.kt
@@ -0,0 +1,31 @@
+package com.cevlikalprn.youneedtodo.data.mapper
+
+import com.cevlikalprn.youneedtodo.domain.model.Priority
+import com.cevlikalprn.youneedtodo.domain.model.ToDoTask
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class TaskEntityMapperTest {
+
+    private lateinit var taskEntityMapper: TaskEntityMapper
+
+    @Before
+    fun setup() {
+        taskEntityMapper = TaskEntityMapper()
+    }
+
+    @Test
+    fun taskEntityMapper_toDoTaskTransformsIntoToDoEntity_returnsTodoEntity() {
+        val toDoTask = ToDoTask(
+            id = 1,
+            title = "title",
+            description = "description",
+            priority = Priority.LOW
+        )
+        val toDoTaskEntity = taskEntityMapper(toDoTask)
+        assertEquals(toDoTask.title, toDoTaskEntity.title)
+        assertEquals(toDoTask.description, toDoTaskEntity.description)
+        assertEquals(toDoTask.priority, toDoTaskEntity.priority)
+    }
+}
